bc088a3418b653f68f9a8a71159a3afc233ec098
alexfernandez05 <92996199+alexfernandez05@users.noreply.github.com>
2021-10-22 18:41:30 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -19,15 +19,20 @@ function myFunction() {
     let dias1 = document.getElementById("dias1").value;
     let dias2 = document.getElementById("dias2").value;
 
-    let diasxpers = ((dias1 - dias2) + (dias2 * 2));
-    let ppd = total / diasxpers;
-     
+    let diasxpers = parseInt(dias1- dias2) + (dias2 * 2);
+    //let ppd = Math.round(( (total / diasxpers) + Number.EPSILON) * 100) / 100;
+    let ppd = (total / diasxpers); 
 
-    text =  'totaldias = ' + diasxpers + '<br>' +
-            'precio/pers/dia = ' + ppd + '<br>' +
-            pers1 + ' '+dias1 + 'dias ' + ppd*dias1 + '€<br>' +
-            pers2 + ' '+dias2 + 'dias ' + ppd*dias2 + '€<br>' +
-            ((ppd*dias1) + (ppd*dias2)) + '€';
+    // text =  'totaldias = ' + diasxpers + '<br>' +
+    //         'precio/pers/dia = ' + ppd + '<br>' +
+    //         pers1 + ' '+ dias1 + 'dias ' + ppd*dias1 + '€<br>' +
+    //         pers2 + ' '+ dias2 + 'dias ' + ppd*dias2 + '€<br>' +
+    //         ((ppd*dias1) + (ppd*dias2)) + '€';
+
+    text =  'precio/pers/dia = ' + Math.round(( (ppd) + Number.EPSILON) * 100) / 100 + '€<br>' +
+            pers1 + ' '+ dias1 + 'dias ' + Math.round(( (ppd*dias1) + Number.EPSILON) * 100) / 100 + '€<br>' +
+            pers2 + ' '+ dias2 + 'dias ' + Math.round(( (ppd*dias2) + Number.EPSILON) * 100) / 100 + '€<br>' +
+            Math.round(( ((ppd*dias1) + (ppd*dias2)) + Number.EPSILON) * 100) / 100 + '€';
 
     document.getElementById("demo").innerHTML = text;
   }
