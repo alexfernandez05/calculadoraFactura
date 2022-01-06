b90eaede3ca85200bc2e3440044c43dfff65aaa5
alexfernandez05 <92996199+alexfernandez05@users.noreply.github.com>
2022-01-06 20:13:18 +0100

    4 persons

--- script/script.js
+++ script/script.js
@@ -1,43 +1,39 @@
 function myFunction() {
-    // Get the value of the input field with id="numb"
-    let total = document.getElementById("total").value;
-    let diasFactura = document.getElementById("diasFactura").value;
+        let total = document.getElementById("total").value;
+        let diasFactura = document.getElementById("diasFactura").value;
 
-    // If x is Not a Number or less than one or greater than 10
-    let text;
+        let text;
 
-    // if (isNaN(total) || total < 1 || total > 100) {
-    //   text = "Input not valid";
-    // } else {
-    //   text = "Input OK";
-    // }
+        let pers1 = document.getElementById("pers1").value;
+        let pers2 = document.getElementById("pers2").value;
+        let pers3 = document.getElementById("pers3").value;
+        let pers4 = document.getElementById("pers4").value;
 
+        let dias1 = document.getElementById("dias1").value;
+        let dias2 = document.getElementById("dias2").value;
+        let dias2 = document.getElementById("dias3").value;
+        let dias2 = document.getElementById("dias4").value;
 
-    let pers1 = document.getElementById("pers1").value;
-    let pers2 = document.getElementById("pers2").value;
+        //let diasxpers = parseInt(dias1 - dias2) + (dias2 * 2);
+        let diasxpers = parseInt(dias1 + dias2 + dias3 + dias4);
+        let ppd = (total / diasxpers);  //Precio x persona día
 
-    let dias1 = document.getElementById("dias1").value;
-    let dias2 = document.getElementById("dias2").value;
+        let ppdRound = Math.round(((ppd) + Number.EPSILON) * 100) / 100;
 
-    let diasxpers = parseInt(dias1- dias2) + (dias2 * 2);
-    //let ppd = Math.round(( (total / diasxpers) + Number.EPSILON) * 100) / 100;
-    let ppd = (total / diasxpers); 
+        p1 = Math.round(((ppd * dias1) + Number.EPSILON) * 100) / 100;
+        p2 = Math.round(((ppd * dias2) + Number.EPSILON) * 100) / 100;
+        p3 = Math.round(((ppd * dias3) + Number.EPSILON) * 100) / 100;
+        p4 = Math.round(((ppd * dias4) + Number.EPSILON) * 100) / 100;
 
-    // text =  'totaldias = ' + diasxpers + '<br>' +
-    //         'precio/pers/dia = ' + ppd + '<br>' +
-    //         pers1 + ' '+ dias1 + 'dias ' + ppd*dias1 + '€<br>' +
-    //         pers2 + ' '+ dias2 + 'dias ' + ppd*dias2 + '€<br>' +
-    //         ((ppd*dias1) + (ppd*dias2)) + '€';
+        //tot = Math.round((((ppd * dias1) + (ppd * dias2)) + Number.EPSILON) * 100) / 100;
+        tot = Math.round((((ppd * dias1) + (ppd * dias2) + (ppd * dias3) + (ppd * dias4)) + Number.EPSILON) * 100) / 100;
 
-    let ppdRound = Math.round(( (ppd) + Number.EPSILON) * 100) / 100;
-    p1 = Math.round(( (ppd*dias1) + Number.EPSILON) * 100) / 100;
-    p2 = Math.round(( (ppd*dias2) + Number.EPSILON) * 100) / 100;
-    tot  = Math.round(( ((ppd*dias1) + (ppd*dias2)) + Number.EPSILON) * 100) / 100;
+        text = 'precio/pers/dia = ' + ppdRound + '€<br>' +
+                pers1 + ' ' + dias1 + 'dias ' + p1 + '€<br>' +
+                pers2 + ' ' + dias2 + 'dias ' + p2 + '€<br>' +
+                pers3 + ' ' + dias3 + 'dias ' + p3 + '€<br>' +
+                pers4 + ' ' + dias4 + 'dias ' + p4 + '€<br>' +
+                tot + '€';
 
-    text =  'precio/pers/dia = ' + ppdRound + '€<br>' +
-            pers1 + ' '+ dias1 + 'dias ' + p1 + '€<br>' +
-            pers2 + ' '+ dias2 + 'dias ' + p2 + '€<br>' +
-            tot + '€';
-
-    document.getElementById("demo").innerHTML = text;
-  }
+        document.getElementById("demo").innerHTML = text;
+}
